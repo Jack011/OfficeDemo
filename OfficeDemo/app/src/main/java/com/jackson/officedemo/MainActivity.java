de79df80898f22f89628d0575b4293c46e04d56e
USER-20160228MT\Administrator <907203644@qq.com>
2016-09-24 18:56:40 +0800

    添加了图片。和图片点击事件

--- OfficeDemo/app/src/main/java/com/jackson/officedemo/MainActivity.java
+++ OfficeDemo/app/src/main/java/com/jackson/officedemo/MainActivity.java
@@ -4,21 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button clickBtn;
-
+    ImageView imageOne,imageTwo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         clickBtn= (Button) findViewById(R.id.click_btn);
+        imageOne=(ImageView) findViewById(R.id.imageOne);
+        imageTwo=(ImageView) findViewById(R.id.imageTwo);
         clickBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                imageOne.setImageResource(R.drawable.d);
                 Toast.makeText(MainActivity.this, "我被点击了，啊，哈哈哈哈哈啊哈哈", Toast.LENGTH_SHORT).show();
             }
         });
+
+        imageOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageTwo.setImageResource(R.drawable.c);
+            }
+        });
     }
+
+
 }
